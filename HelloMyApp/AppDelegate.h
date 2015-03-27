e4bab38745dc059b9e70ffbab4f8d837b27d4ef8
Apra <apra.ypsilon@gmail.com>
2015-03-27 21:03:44 +0530

    Initial Commit

---
+++ HelloMyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloMyApp
+//
+//  Created by universal informatics on 27/03/15.
+//  Copyright (c) 2015 universal informatics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloMyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloMyApp
+//
+//  Created by universal informatics on 27/03/15.
+//  Copyright (c) 2015 universal informatics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
