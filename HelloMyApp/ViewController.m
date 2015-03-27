d716f9b492df4046ab16a2657294c0d8547a8537
Apra <apra.ypsilon@gmail.com>
2015-03-27 21:20:30 +0530

    final message

--- HelloMyApp/ViewController.m
+++ HelloMyApp/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //hello world
+    NSLog(@"my name is akhilesh");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
